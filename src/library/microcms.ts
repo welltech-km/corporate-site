59274b816859f06820ae8d8ae36e1474aeb5ba6c
Kensei Maekawa <kensei_maekawa@wellness-technology.com>
2024-08-15 15:22:29 +0900

    ⚡️ first commit

---
+++ src/library/microcms.ts
@@ -0,0 +1,40 @@
+//SDK利用準備
+import type { MicroCMSQueries } from "microcms-js-sdk";
+import { createClient } from "microcms-js-sdk";
+
+const client = createClient({
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
+});
+
+//型定義
+export type News = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  content: string;
+};
+export type NewsResponse = {
+  totalCount: number;
+  offset: number;
+  limit: number;
+  contents: News[];
+};
+
+//APIの呼び出し
+export const getNews = async (queries?: MicroCMSQueries) => {
+  return await client.get<NewsResponse>({ endpoint: "news", queries });
+};
+export const getNewsDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  return await client.getListDetail<News>({
+    endpoint: "news",
+    contentId,
+    queries,
+  });
+};
